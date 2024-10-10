49efef18e46b9ff363421fc46224b49c13bf80c9
wjpiantkoski <wjpiantkoski@gmail.com>
2024-10-10 10:54:12 -0300

    feat: add sign in action

--- src/app/(auth)/actions.ts
+++ src/app/(auth)/actions.ts
@@ -2,8 +2,10 @@
 
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
-import { signupSchema } from "@/lib/validation-schemas/auth/signup.schema";
 import signupUsecase from "@/usecases/auth/signup.usecase";
+import { signupSchema } from "@/lib/validation-schemas/auth/signup.schema";
+import { signinSchema } from "@/lib/validation-schemas/auth/signin.schema";
+import signinUsecase from "@/usecases/auth/signin.usecase";
 
 export async function signUp(formData: FormData) {
   const validated = signupSchema.safeParse({
@@ -17,6 +19,23 @@ export async function signUp(formData: FormData) {
   }
 
   const sessionCookie = await signupUsecase(validated.data);
+
+  cookies().set("session", sessionCookie.serialize());
+
+  redirect("/");
+}
+
+export async function signIn(formData: FormData) {
+  const validated = signinSchema.safeParse({
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  });
+
+  if (!validated.success) {
+    throw new Error("Invalid form data");
+  }
+
+  const sessionCookie = await signinUsecase(validated.data);
 
   cookies().set("session", sessionCookie.serialize());
 
